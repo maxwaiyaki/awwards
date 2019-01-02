0e71a503a6f14eddcc38b68819c0cad0db352117
maxwaiyaki <maxwel.wairimu@gmail.com>
2019-01-02 20:21:51 +0300

    Add API urls

--- design/urls.py
+++ design/urls.py
@@ -12,3 +12,15 @@ urlpatterns=[
     url(r'^site/(\d+)',views.site,name='site'),
     url(r'^search/',views.search_results, name='search_results'),
     url(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+
+    url(r'^api/profiles/$', views.ProfileList.as_view()),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
+    url(r'^api/categories/$', views.categoriesList.as_view()),
+    url(r'^api/countries/$', views.countriesList.as_view()),
+    url(r'^api/technologies/$', views.technologiesList.as_view()),
+    url(r'^api/colors/$', views.colorsList.as_view()),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
