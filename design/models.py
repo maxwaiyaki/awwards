49a2ab37f33108da4ea44acc5d810f74b97a3424
maxwaiyaki <maxwel.wairimu@gmail.com>
2019-01-02 19:34:54 +0300

    Add Project class

--- design/models.py
+++ design/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    landing_page = models.ImageField(upload_to='landingpage/')
+    description = HTMLField()
+    link= models.CharField(max_length=255)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    screenshot1 = models.ImageField(upload_to='screenshots/')
+    screenshot2 = models.ImageField(upload_to='screenshots/')
+    screenshot3 = models.ImageField(upload_to='screenshots/')
+    screenshot4 = models.ImageField(upload_to='screenshots/')
+    design = models.IntegerField(blank=True,default=0)
+    usability = models.IntegerField(blank=True,default=0)
+    creativity = models.IntegerField(blank=True,default=0)
+    content = models.IntegerField(blank=True,default=0)
+    overall_score = models.IntegerField(blank=True,default=0)
+    country = models.ForeignKey(countries,on_delete=models.CASCADE)
+    technologies = models.ManyToManyField(technologies)
+    categories = models.ManyToManyField(categories)
+    colors = models.ManyToManyField(colors)
+    post_date = models.DateTimeField(auto_now_add=True)
+    avatar = models.ImageField(upload_to='avatars/')
