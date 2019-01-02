969cebf456e6d952aa4f8c491fc7898e78b273fe
maxwaiyaki <maxwel.wairimu@gmail.com>
2019-01-02 21:06:36 +0300

    Register Design models

--- design/admin.py
+++ design/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register(categories)
+admin.site.register(technologies)
+admin.site.register(colors)
+admin.site.register(countries)
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Profile)
+admin.site.register(Rating)
