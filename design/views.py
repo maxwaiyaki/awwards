f86cc19eeb3b5759fa8ad0ef0b317e3b034715ca
maxwaiyaki <maxwel.wairimu@gmail.com>
2019-01-02 20:42:23 +0300

    Add user profile views

--- design/views.py
+++ design/views.py
@@ -140,3 +140,11 @@ def search_results(request):
     else:
         message="You haven't searched for any term"
         return render(request,'search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    user = User.objects.get(username=username)
+    profile =Profile.objects.get(username=user)
+    projects=Project.objects.filter(username=user)
+
+    return render(request,'user-profile.html',{"projects":projects,"profile":profile})
