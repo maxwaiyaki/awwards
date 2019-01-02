ecbe8349c078175d92aa422e0873810f00be2579
maxwaiyaki <maxwel.wairimu@gmail.com>
2019-01-02 19:39:20 +0300

    Add Profile class and method

--- design/models.py
+++ design/models.py
@@ -31,3 +31,14 @@ class Project(models.Model):
         # projects = cls.objects.filter(Q(username__username=search_term) | Q(title__icontains=search_term) | Q(colors__colors=search_term) | Q(technologies__technologies=search_term) | Q(categories__categories=search_term) | Q(country__countries=search_term))
         projects = cls.objects.filter(Q(username__username=search_term) | Q(title__icontains=search_term) | Q(country__countries=search_term) | Q(overall_score__icontains=search_term))
         return projects
+
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    description = HTMLField()
+    country = models.ForeignKey(countries,on_delete=models.CASCADE)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
