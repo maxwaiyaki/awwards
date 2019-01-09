cdd68fbcf14a2b9fc58c00258080a34d0f97b6d0
maxwaiyaki <maxwel.wairimu@gmail.com>
2019-01-09 14:44:30 +0300

    Add Project Tests

--- design/views.py
+++ design/views.py
@@ -20,20 +20,20 @@ from .serializer import ProfileSerializer,ProjectSerializer,technologiesSerializ
 def index(request):
     date = dt.date.today()
     winners=Project.objects.all()[:4]
-    caraousel = Project.objects.order_by('-overall_score')[0]
+    caraousel = Project.objects.order_by('-overall_score')[2]
     nominees=Project.objects.all()[4:8]
-    directories=Project.objects.all()[8:11]
-    resources=Project.objects.all()[11:15]
+    directories=Project.objects.all()[8:12]
+    resources=Project.objects.all()[12:15]
     resources2=Project.objects.all()[15:19]
 
-    try:
-        if not request.user.is_authenticated:
-            return redirect('/accounts/login/')
-        current_user = request.user
-        profile =Profile.objects.get(username=current_user)
-        print(current_user)
-    except ObjectDoesNotExist:
-        return redirect('create-profile')
+    # try:
+    #     if not request.user.is_authenticated:
+    #         return redirect('/accounts/login/')
+    #     current_user = request.user
+    #     profile =Profile.objects.get(username=current_user)
+    #     print(current_user)
+    # except ObjectDoesNotExist:
+    #     return redirect('create-profile')
 
     return render(request,'index.html',{"winners":winners,"profile":profile,"caraousel":caraousel, "date":date,"nominees":nominees,"directories":directories,"resources":resources,"resources2":resources2})
 
@@ -68,7 +68,7 @@ def new_project(request):
             project.save()
     else:
         form = ProjectForm()
-
+        # return HttpResponseRedirect('/')
     return render(request,'new_project.html',{"form":form})
 
 def directory(request):
